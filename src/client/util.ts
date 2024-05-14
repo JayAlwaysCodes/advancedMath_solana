8d4f68f0fa017e2126d44b20282816cf872f2e0f
jayalwayscodes <johnsonjardocs@gmail.com>
2024-05-14 07:36:57 +0100

    created

---
+++ src/client/util.ts
@@ -0,0 +1,8 @@
+import{Keypair,} from '@solana/web3.js';
+import * as fs from 'mz/fs';
+
+export async function createKeypairFromFile(filePath:string,): Promise<Keypair> {
+    const secretKeyString = await fs.readFile(filePath, {encoding: 'utf8'});
+    const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+    return Keypair.fromSecretKey(secretKey);
+}
