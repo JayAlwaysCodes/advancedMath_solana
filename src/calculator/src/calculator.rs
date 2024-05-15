d3ea4e19c678928fbbb6355d5803b271d1b99a05
jayalwayscodes <johnsonjardocs@gmail.com>
2024-05-15 18:01:52 +0100

    added

---
+++ src/calculator/src/calculator.rs
@@ -0,0 +1,18 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct CalculatorInstructions{
+    operation: u32,
+    operating_value: u32,
+}
+
+impl CalculatorInstructions {
+    pub fn evaluate(self, value: u32) -> u32{
+        match &self.operation{
+            1 => value + &self.operating_value,
+            2 => value - &self.operating_value,
+            3 => value * &self.operating_value,
+            _ => value * 0,
+        }
+    }
+}
