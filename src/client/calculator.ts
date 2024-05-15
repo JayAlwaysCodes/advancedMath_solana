eff11c07923291746af4dc73057462f25e241926
jayalwayscodes <johnsonjardocs@gmail.com>
2024-05-15 18:02:45 +0100

    added

---
+++ src/client/calculator.ts
@@ -0,0 +1,32 @@
+import * as borsh from 'borsh';
+import * as math from './math';
+
+class Calculator {
+    value = 0;
+    constructor(fields: {value: number} | undefined = undefined){
+        if(fields){
+            this.value = fields.value;
+        }
+    }
+}
+
+const CalculatorSchema = new Map([
+    [Calculator, {kind: 'struct', fields: [['value', 'u32']]}],
+]);
+
+const CALCULATOR_SIZE = borsh.serialize(
+    CalculatorSchema,
+    new Calculator(),
+).length;
+
+async function main() {
+    await math.example('sum', CALCULATOR_SIZE);
+}
+
+main().then(
+    () => process.exit(),
+    err => {
+        console.error(err);
+        process.exit(-1);
+    },
+);
