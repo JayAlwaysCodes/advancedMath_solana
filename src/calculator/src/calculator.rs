3d0e0e369688ee886606c31e5571c0883d3a44f3
jayalwayscodes <johnsonjardocs@gmail.com>
2024-05-16 19:23:20 +0100

    modified

--- src/calculator/src/calculator.rs
+++ src/calculator/src/calculator.rs
@@ -1,14 +1,16 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 
+
+
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct CalculatorInstructions{
+pub struct CalculatorInstructions {
     operation: u32,
     operating_value: u32,
 }
 
 impl CalculatorInstructions {
-    pub fn evaluate(self, value: u32) -> u32{
-        match &self.operation{
+    pub fn evaluate(self, value: u32) -> u32 {
+        match &self.operation {
             1 => value + &self.operating_value,
             2 => value - &self.operating_value,
             3 => value * &self.operating_value,
